d3d61690f931592df7487c7c65dd3580361c5626
ChandyCharmstrong <132002656+ChandyCharmstrong@users.noreply.github.com>
2023-06-21 11:00:47 +0100

    Add files via upload
    Breaking into the world of python

---
+++ Hello_World.py
@@ -0,0 +1,8 @@
+# Compulsory task 1 
+name = input("Please enter your name:")
+print("Congratualtions! Your name is " + name)
+age = input("Please enter your age:")
+print("Congratualtions! Your age is " + age)
+print("")
+print("Hello World!")
+
